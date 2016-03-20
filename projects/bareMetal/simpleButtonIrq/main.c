cc3ac70eb9507d6057e0ccbe56339b7592adcff4
callizta <niedermeier.anja@gmail.com>
2016-03-20 23:02:30 +0100

    Switch on LED from Button through PPI

--- projects/bareMetal/simpleButtonIrq/main.c
+++ projects/bareMetal/simpleButtonIrq/main.c
@@ -47,27 +47,38 @@ int main(void)
 	
 	// Configure MY_BUTTON_1 as input	
 	NRF_GPIO->PIN_CNF[MY_BUTTON_1] = (GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos) |
-                                   (GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos) |
-                                   (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos) |
-                                   (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos) |
-                                   (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos);
-	
+                                   (GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos) //|
+                                   //(GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos) |
+                                   | (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
+                                   //(GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos);
+																	;
 	
 	// Configure GPIOTE_CHANNEL_NUMBER to generate an event when MY_BUTTON_1 is pushed
 	NRF_GPIOTE->CONFIG[GPIOTE_CHANNEL_NUMBER] = ( GPIOTE_CONFIG_MODE_Event << GPIOTE_CONFIG_MODE_Pos ) | 
 																							( MY_BUTTON_1 << GPIOTE_CONFIG_PSEL_Pos ) |
 																							( GPIOTE_CONFIG_POLARITY_HiToLo << GPIOTE_CONFIG_POLARITY_Pos ) ;
 	
+	NRF_GPIOTE->CONFIG[1] = ( GPIOTE_CONFIG_MODE_Task << GPIOTE_CONFIG_MODE_Pos ) | 
+																							( MY_LED_2 << GPIOTE_CONFIG_PSEL_Pos ) |
+																							( GPIOTE_CONFIG_POLARITY_Toggle << GPIOTE_CONFIG_POLARITY_Pos ) ;
+	
 		
 	NRF_GPIOTE->INTENSET = GPIOTE_INTENSET_IN0_Set << GPIOTE_INTENSET_IN0_Pos;
 	
-	NVIC_EnableIRQ(GPIOTE_IRQn);
+	//NVIC_EnableIRQ(GPIOTE_IRQn);
 		
 	// switch on LED_1
 	NRF_GPIO->OUTSET = (1UL << MY_LED_1);
 	
 	// switch off LED_2
-	NRF_GPIO->OUTSET = (0UL << MY_LED_2);
+	//NRF_GPIO->OUTSET = (0UL << MY_LED_2);
+	
+	// Configure PPI channel 0 to start Watchdog timer when GPIOTE fires event 0
+    NRF_PPI->CH[0].EEP = (uint32_t)(&NRF_GPIOTE->EVENTS_IN[0]);
+    NRF_PPI->CH[0].TEP = (uint32_t)(&NRF_GPIOTE->TASKS_OUT[1]);
+	
+	// Enable only PPI channel 0.
+    NRF_PPI->CHEN = (PPI_CHEN_CH0_Enabled << PPI_CHEN_CH0_Pos);
 	
 	
 	while(1)
